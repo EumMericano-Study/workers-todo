f9e87262c6990d99e2f337a0b05b1467d7347f55
Eumericano <opwer032@naver.com>
2022-03-21 23:25:27 +0900

    feat: Combine test codes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { handleRequest } from './handler'
-
 addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request))
 })
+
+export async function handleRequest(request: Request): Promise<Response> {
+  return new Response(`request method: ${request.method}`)
+}
